7a416d282702bf097c0f6f3e9148183d53b3ca1e
Luiz Henrique Santos Sousa Diniz <lhenrique_diniz@hotmail.com>
2017-03-29 18:35:42 -0300

    implementação 1.0

---
+++ Lista de Exercicios/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lista_de_Exercicios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //exercício 1
+            /*
+            Console.WriteLine("É PRECISO FAZER TODOS OS EXERCÍCIOS PARA APRENDER");
+            Console.ReadLine();             
+             */
+            //exercício 2
+            /*
+            Console.WriteLine("Luiz Henrique Santos Sousa Diniz");
+            Console.ReadLine();                          
+             */
+            //exercício 3
+            /*
+            Console.WriteLine(28*43);
+            Console.ReadLine();
+            */
+            //exercício 4
+            /*
+            Console.WriteLine((9+7+8)/3);
+            Console.ReadLine();             
+             */
+            //exercício 5
+            /*
+            int a;
+            a=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a);
+            Console.ReadLine();
+             
+             */
+            //exercício 6
+            /*
+            int a,b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a+" "+b);
+            Console.ReadLine();             
+             */
+            //exercício 7
+            /*
+            int a;
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine((a-1) + " " + (a+1));
+            Console.ReadLine();
+             
+             */
+            //exercício 8
+            /*
+            string a,b,c;
+            a = Console.ReadLine();
+            b = Console.ReadLine();
+            c = Console.ReadLine();
+            Console.WriteLine((a)+"\n"+b+"\n"+c);
+            Console.ReadLine();
+             
+             */
+            //exercício 9
+            /*
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Soma "+(a  + b));
+             Console.ReadLine();
+             */
+            //exercício 10
+            /*
+            int a, b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("produto " + (a *b));
+            Console.ReadLine();
+             */
+            //exercício 11
+            /*
+            decimal a;
+            a = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine( a/3);
+            Console.ReadLine();
+             
+             */
+            //exercício 12
+            /*
+            decimal a,b;
+            a = Convert.ToDecimal(Console.ReadLine());
+            b = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine( ("Media "+(b+a)/2));
+            Console.ReadLine();
+             
+             */
+            //exercício 13
+            /*
+             decimal a;
+            a = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine(a*1.01m);
+            Console.ReadLine();
+             
+             */
+            //exercício 14
+            /*
+             vide o exercício 12
+             
+             */
+            //exercício 15
+            /*
+            string a;
+            a = Console.ReadLine();
+            Console.WriteLine("Todo o nome: " + a);
+            Console.WriteLine("Primeiro Caractere: " + a.Substring(0,1));
+            Console.WriteLine("Ultimo Caractere: " + a.Substring(a.Length-1));
+            Console.WriteLine("Do Primeiro ate o Terceiro: " + a.Substring(0, 3));
+            Console.WriteLine("Quarto Caractere: " + a.Substring(3, 1));
+            Console.WriteLine("Todos menos o Primeiro: " + a.Substring( 1));
+            Console.WriteLine("Os Dois Últimos: " + a.Substring(a.Length - 2, 2));
+            Console.ReadLine();
+            */
+        }
+    }
+}
